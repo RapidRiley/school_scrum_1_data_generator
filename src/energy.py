5049c378431b734f5d15e5b6a71c2dac10a146e4
Riley <rileywarps@gmail.com>
2022-03-04 10:06:46 +0100

    fix: Removed duplicate variable

--- src/energy.py
+++ src/energy.py
@@ -4,7 +4,6 @@ class Energy:
 	def __init__(self, range_min: int, range_max: int) -> None:
 		self.at_start: int = random.randrange(range_min, range_max)
 		self.at_end: int = random.randrange(range_min, range_max)
-		self.at_collision: int = random.randrange(0, self.at_start)
 		self.at_collision: int = self.at_start + random.randrange(0, range_max - self.at_start)
  
 	def __repr__(self) -> str:
