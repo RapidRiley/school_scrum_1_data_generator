067d587e0994ea301ba4b77dcbeb59d4f2dfea06
Riley <rileywarps@gmail.com>
2022-03-09 16:23:52 +0100

    fix: Fixed line endings *again*

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,9 @@
 while [ true ]
 do
 	SLEEP_TIMER=$(shuf -i 60-15400 -n 1)
-	
+
 	python3 /app/main.py
 
 	echo "Batch created, sleeping for $SLEEP_TIMER seconds."
-	sleep $SLEEP_TIMER; 
+	sleep $SLEEP_TIMER;
 done
