d2d38108e56dbfa6844e4e67c3ce95e5a587fd27
RapidRiley <98483809+RapidRiley@users.noreply.github.com>
2022-03-09 16:49:14 +0100

    Recreated script with correct line endings

---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+while [ true ]
+do
+	SLEEP_TIMER=$(shuf -i 60-15400 -n 1)
+
+	python3 /app/main.py
+
+	echo "Batch created, sleeping for $SLEEP_TIMER seconds."
+	sleep $SLEEP_TIMER;
+done
