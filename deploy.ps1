edff45e1d073e6b0d542928660881404b0a2d366
Riley <rileywarps@gmail.com>
2022-03-09 15:46:40 +0100

    feat: Added shared volume-related commands.

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,12 @@
 $PORT=4000
 
-docker build -t generator `
+docker volume create --name cern_data
+
+docker build -t school_1_scrum_data_generator `
     --build-arg DATA_DIR="/data" `
     --build-arg SERVER_PORT=${PORT} .
 
 if($?)
 {
-    docker run -td generator
+    docker run -d --mount 'type=volume,src=cern_data,dst=/data' --name school_1_scrum_data_generator -t school_1_scrum_data_generator
 }
